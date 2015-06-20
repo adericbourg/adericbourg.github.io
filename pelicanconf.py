e8c34af2ea0a64d4f129ed4096222b2ebefe9fec
Alban Dericbourg <alban@dericbourg.net>
2015-06-20 13:21:06 +0200

    Theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'fr'
 
+THEME = 'themes/pelican-themes/foundation-default-colours'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
