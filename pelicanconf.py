b16653577f68da410c384b76e9c27a4caddec501
Alban Dericbourg <alban@dericbourg.net>
2015-06-23 20:43:03 +0200

    Fork theme "gum".

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,7 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'fr'
 
-THEME = 'themes/gum'
+THEME = 'theme'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -22,15 +22,10 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Mon CV', 'https://github.com/adericbourg/CV'),
-)
+GITHUB_URL = 'https://github.com/adericbourg'
+TWITTER_URL = 'https://twitter.com/adericbourg'
 
 # Social widget
-SOCIAL = (('Twitter', 'https://twitter.com/adericbourg'),
-          ('GitHub', 'https://github.com/adericbourg'),
-          ('LinkedIn', 'https://www.linkedin.com/in/adericbourg'),
-)
 
 DEFAULT_PAGINATION = 10
 
